6293a26a89678cd866e8e32b76fa0d584c0b82b6
Dani <danielreschkelima@gmail.com>
2026-07-19 22:51:21 -0300

    Botão excluir services

--- React/costs/src/components/pages/Project.js
+++ React/costs/src/components/pages/Project.js
@@ -108,7 +108,29 @@ function Project() {
 
     }
 
-    function removeService() {
+    function removeService(id, cost) {
+
+        const servicesUpdated = project.services.filter((service) => service.id !== id);
+
+        const projectUpdated = project
+
+        projectUpdated.services = servicesUpdated;
+        projectUpdated.cost = parseFloat(projectUpdated.cost) - parseFloat(cost);
+
+        fetch(`http://localhost:5000/projects/${projectUpdated.id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application.json'
+            },
+            body: JSON.stringify(projectUpdated)
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            setProject(projectUpdated);
+            setServices(servicesUpdated);
+            setMessage('Serviço removido com sucesso!');
+        })
+        .catch((err) => console.log(err))
 
     }
 
@@ -160,7 +182,7 @@ function Project() {
                         <Container customClass="start">
                             {services.length > 0 &&
                                 services.map((service) => (
-                                    <ServiceCard id={service.id} name={service.name} cost={service.cost} description={service.description} key={service.key} handdleRemove={removeService} />
+                                    <ServiceCard id={service.id} name={service.name} cost={service.cost} description={service.description} key={service.key} handleRemove={removeService} />
                                 ))
                             }
                             {services.length === 0 && <p>Não há serviços cadastrados</p>}
