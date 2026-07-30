8a89933b6d914b058dc5d37821cceecfd05fe71d
Danniiii <danielreschkelima@gmail.com>
2026-07-30 16:55:53 -0300

    Forms

--- Django/learning_log/learning_logs/views.py
+++ Django/learning_log/learning_logs/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Topic 
+from .forms import TopicForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 def index(request):
     """Página principal do Learning_Log"""
@@ -17,3 +20,17 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Adiciona um novo assunto."""
+    if request.method != 'POST':
+        #Nenhum dado cria o form em branco
+        form = TopicForm()
+    else:
+        #Dados de POST submetidos; processa os dados.
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('topics'))
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
