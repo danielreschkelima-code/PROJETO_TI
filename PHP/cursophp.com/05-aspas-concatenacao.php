26ebf48404e007e6a4d58140221d0a238c908d07
Danniiii <danielreschkelima@gmail.com>
2026-08-13 14:14:30 -0300

    Aspas

---
+++ PHP/cursophp.com/05-aspas-concatenacao.php
@@ -0,0 +1,5 @@
+<?php
+$nome = 'Daniel'; // ' literal
+echo 'Meu nome é $nome (aqui vai sair o nome da var mesmo)'.$nome.' e minha idade é \'18.\''; // concatenação tipo 1
+echo '<hr>';
+echo "Meu nome é \"$nome\"."; // " é interpretativa
